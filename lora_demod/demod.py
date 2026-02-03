7646b3a05cc73e937682127ff66eb90bcbe24fa0
JaiNana123321 <jai.nana.rsa@gmail.com>
2026-02-03 23:49:45 +0000

    Print lora packets

--- lora_demod/demod.py
+++ lora_demod/demod.py
@@ -1,10 +1,10 @@
 from gnuradio import gr
+from gnuradio import blocks
 import sys
 import signal
 import numpy as np
 import gnuradio.lora_sdr as lora_sdr
 from gnuradio import soapy
-
 
 class lora_RX(gr.top_block):
     def __init__(self):
@@ -14,32 +14,14 @@ class lora_RX(gr.top_block):
         # Variables
         ##################################################
         self.soft_decoding = soft_decoding = True
-
-        # 128 chips per symbol => 2^SF = 128 => SF = 7
         self.sf = sf = 7
-
-        # RF center frequency
         self.center_freq = center_freq = 434.5e6
-
-        # LoRa BW (most common); if decode fails try 250000 or 500000
         self.bw = bw = 125000
-
-        # HackRF-friendly sampling rate; keep (samp_rate / bw) integer
-        self.samp_rate = samp_rate = 2_000_000  # 2e6 / 125e3 = 16
-
-        # Coding rate 4/5 => cr = 1 (gr-lora_sdr convention)
+        self.samp_rate = samp_rate = 2_000_000 
         self.cr = cr = 1
-
-        # Explicit header mode => impl_head = False
         self.impl_head = impl_head = False
-
-        # CRC disabled
         self.has_crc = has_crc = False
-
-        # Payload max (explicit header contains real length)
         self.pay_len = pay_len = 256
-
-        # Sync word: 0x1E
         self.sync_word = sync_word = 0x1E
 
         ##################################################
@@ -48,64 +30,40 @@ class lora_RX(gr.top_block):
         device = "driver=hackrf"
         dtype = "fc32"
         nchan = 1
-        dev_args = ""       # e.g. "serial=..." if needed
+        dev_args = ""
         stream_args = ""
-        tune_args = [""]    # one entry per channel
+        tune_args = [""]
         other_settings = [""]
-
-        self.soapy_source_0 = soapy.source(
-            device, dtype, nchan,
-            dev_args, stream_args,
-            tune_args, other_settings
-        )
-
-        # Set sample rate / freq / bw after construction
+        
+        self.soapy_source_0 = soapy.source(device, dtype, nchan, dev_args, stream_args, tune_args, other_settings)
         self.soapy_source_0.set_sample_rate(0, samp_rate)
         self.soapy_source_0.set_frequency(0, center_freq)
         self.soapy_source_0.set_bandwidth(0, bw)
-
-        # Start with moderate gain; adjust if too quiet/clipping
         self.soapy_source_0.set_gain(0, 20)
-
-        # Buffer sizing similar to UHD version
         self.soapy_source_0.set_min_output_buffer(int(np.ceil(samp_rate / bw * (2**sf + 2))))
 
         ##################################################
         # LoRa SDR blocks
         ##################################################
-        self.lora_sdr_header_decoder_0 = lora_sdr.header_decoder(
-            impl_head, cr, pay_len, has_crc,
-            False,
-            True   # verify explicit header checksum
-        )
-
+        self.lora_sdr_header_decoder_0 = lora_sdr.header_decoder(impl_head, cr, pay_len, has_crc, False, True)
         self.lora_sdr_hamming_dec_0 = lora_sdr.hamming_dec(soft_decoding)
         self.lora_sdr_gray_mapping_0 = lora_sdr.gray_mapping(soft_decoding)
-
-        self.lora_sdr_frame_sync_0 = lora_sdr.frame_sync(
-            int(center_freq),
-            bw,
-            sf,
-            impl_head,
-            [sync_word],
-            int(samp_rate / bw),
-            8
-        )
-
+        self.lora_sdr_frame_sync_0 = lora_sdr.frame_sync(int(center_freq), bw, sf, impl_head, [sync_word], int(samp_rate / bw), 8)
         self.lora_sdr_fft_demod_0 = lora_sdr.fft_demod(soft_decoding, True)
         self.lora_sdr_dewhitening_0 = lora_sdr.dewhitening()
         self.lora_sdr_deinterleaver_0 = lora_sdr.deinterleaver(soft_decoding)
-
-        # CRC block can remain; since has_crc=False, CRC won't be required
         self.lora_sdr_crc_verif_0 = lora_sdr.crc_verif(1, False)
+        
+        # Added to print packets to terminal
+        self.blocks_message_debug_0 = blocks.message_debug()
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect(
-            (self.lora_sdr_header_decoder_0, 'frame_info'),
-            (self.lora_sdr_frame_sync_0, 'frame_info')
-        )
+        self.msg_connect((self.lora_sdr_header_decoder_0, 'frame_info'), (self.lora_sdr_frame_sync_0, 'frame_info'))
+        
+        # Packet Printing Connection
+        self.msg_connect((self.lora_sdr_crc_verif_0, 'out'), (self.blocks_message_debug_0, 'print'))
 
         self.connect((self.soapy_source_0, 0), (self.lora_sdr_frame_sync_0, 0))
         self.connect((self.lora_sdr_frame_sync_0, 0), (self.lora_sdr_fft_demod_0, 0))
@@ -115,7 +73,6 @@ class lora_RX(gr.top_block):
         self.connect((self.lora_sdr_hamming_dec_0, 0), (self.lora_sdr_header_decoder_0, 0))
         self.connect((self.lora_sdr_header_decoder_0, 0), (self.lora_sdr_dewhitening_0, 0))
         self.connect((self.lora_sdr_dewhitening_0, 0), (self.lora_sdr_crc_verif_0, 0))
-
 
 def main():
     tb = lora_RX()
@@ -135,7 +92,6 @@ def main():
         pass
     tb.stop()
     tb.wait()
-
 
 if __name__ == "__main__":
     main()
